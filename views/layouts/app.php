11ca40bf07784925c083522ba9846f95de5cd8a5
aungkyawth3t <aungkyawthethimself@gmail.com>
2026-01-28 19:02:39 +0630

    pull

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="A minimalist space to publish your thoughts, essays, and ideas without distractions.">
     <title>Umbra</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
